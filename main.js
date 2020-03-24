4697fe198e1a32adfc546546873486ab60dde525
Z. Patrick Lewis <patrick_lewis@unc.edu>
2020-03-24 01:38:31 -0400

    added vs code activity bar icons

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 'use strict';
 
-const { app, BrowserWindow, nativeTheme } = require('electron');
+const { app, BrowserWindow, globalShortcut, nativeTheme } = require('electron');
+const os = require('os');
+const osType = process.platform || os.type().toLowerCase();
 
 function createWindow () {
   // Create the browser window.
   // https://www.electronjs.org/docs/api/browser-window
   const win = new BrowserWindow({
+    center: true,
+    minHeight: 600,
     minWidth: 600,
     width: 800,
     height: 600,
+    title: 'Hakchi',
+    // https://www.electronjs.org/docs/api/frameless-window#hiddeninset
+    titleBarStyle: 'hidden',
+    // titleBarStyle: 'hidden',
     webPreferences: {
       nodeIntegration: true
     }
@@ -29,19 +37,34 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+// .then() returns a Promise, so it's chainable, meaning you can call something
+// else afterwards
+app.whenReady().then(createWindow).then(
+    () => {
+        // Register a 'CommandOrControl+Y' shortcut listener.
+        // https://www.electronjs.org/docs/api/accelerator#accelerator
+        globalShortcut.register('CommandOrControl+R', () => {
+          // Do stuff when R and either Command/Control is pressed; this
+          // accelerator prevents refreshing the app and messing up the styling
+          // May need to specify a background color though...
+        });
+    }
+);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  // if (process.platform !== 'darwin') {
+  console.log('osType: ' + osType);
+  if (osType !== 'darwin') {
     app.quit();
   }
 });
 
 // Listen for dark mode and use it!
 // https://www.electronjs.org/docs/tutorial/mojave-dark-mode-guide
+// TODO: How do you set up an app theme properly?
 nativeTheme.on('updated', function theThemeHasChanged () {
     console.log('nativeTheme.shouldUseDarkColors: ' + (nativeTheme.shouldUseDarkColors === true ? 'true' : 'false'));
     // updateMyAppTheme(nativeTheme.shouldUseDarkColors);
